0a67cc55c81ceda8a05d4fbd4fa92828edad2488
Félix Saparelli <felix@passcod.name>
2017-09-03 22:22:43 +1200

    Normalis experientia fabrica

--- src/parse/normalise.rs
+++ src/parse/normalise.rs
@@ -28,3 +28,99 @@ pub fn normalise(input: String) -> String {
     .collect::<Vec<String>>()
     .join(" ")
 }
+
+#[cfg(test)]
+macro_rules! test_normaliser {
+    ($input:expr, $output:expr) =>
+    (assert_eq!(normalise(format!("{}", $input)), $output))
+}
+
+#[cfg(test)]
+macro_rules! test_spaces {
+    ($input:expr) =>
+    (test_normaliser!(format!("x{}x", $input), "x x"))
+}
+
+#[test]
+fn spaces() {
+    // Usual spaces
+    test_spaces!("\n");
+    test_spaces!("\t");
+    test_spaces!(" ");
+    test_spaces!("　"); // IDEOGRAPHIC SPACE
+
+    // Unusual spaces
+    test_spaces!(" "); // NBSP
+    test_spaces!(" "); // EN QUAD
+    test_spaces!(" "); // EM QUAD
+    test_spaces!(" "); // EN SPACE
+    test_spaces!(" "); // EM SPACE
+    test_spaces!(" "); // FIGURE SPACE
+
+    // Rare spaces
+    test_spaces!("\u{85}");
+    test_spaces!("\u{2004}");
+    test_spaces!("\u{2005}");
+    test_spaces!("\u{2006}");
+    test_spaces!("\u{2008}");
+    test_spaces!("\u{2009}");
+    test_spaces!("\u{200A}");
+    test_spaces!("\u{202F}");
+    test_spaces!("\u{205F}");
+
+    // Space collapsing
+    test_spaces!("      ");
+    test_spaces!(" \n\t ");
+}
+
+#[test]
+fn double_quotes() {
+    test_normaliser!("\"", "\"");
+    test_normaliser!("“", "\"");
+    test_normaliser!("”", "\"");
+    test_normaliser!("«", "\"");
+    test_normaliser!("»", "\"");
+}
+
+#[test]
+fn single_quotes() {
+    test_normaliser!("'", "'");
+    test_normaliser!("‘", "'");
+    test_normaliser!("’", "'");
+    test_normaliser!("‹", "'");
+    test_normaliser!("›", "'");
+}
+
+#[test]
+fn brackets() {
+    test_normaliser!("(", "(");
+    test_normaliser!("{", "(");
+    test_normaliser!("[", "(");
+
+    test_normaliser!(")", ")");
+    test_normaliser!("}", ")");
+    test_normaliser!("]", ")");
+}
+
+#[test]
+fn equals() {
+    test_normaliser!("=", "=");
+    test_normaliser!(":", "=");
+    test_normaliser!("≈", "=");
+}
+
+#[test]
+fn dashes() {
+    test_normaliser!("-", "-");
+    test_normaliser!("--", "-");
+    test_normaliser!("—", "-");
+    test_normaliser!("–", "-");
+    test_normaliser!("‒", "-");
+    test_normaliser!("⁓", "-");
+    test_normaliser!("ー", "-");
+    test_normaliser!("―", "-");
+    test_normaliser!("⸺", "-");
+    test_normaliser!("⸻", "-");
+    test_normaliser!("〜", "-");
+    test_normaliser!("～", "-");
+}
